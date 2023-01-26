956514356228a09c5d6b0f4520cb00f0d706faa4
Samara Bastos <samarasampaio30@gmail.com>
2023-01-26 10:59:03 -0300

    Requisito 16 ao 18 - Footer

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Drinks() {
@@ -7,6 +8,7 @@ function Drinks() {
       <Header
         title="Drinks"
       />
+      <Footer />
     </div>
   );
 }
